a192a940ff100c0401863d700c56606aef340676
JDORTIZ-704 <davidaviddavid3@gmail.com>
2026-03-17 22:45:51 -0500

    Aqui sigo

--- src/cursojava/estructurasbasicas/masejercicios/sentenciacondicional/ejercicios/Ejercicio_13.java
+++ src/cursojava/estructurasbasicas/masejercicios/sentenciacondicional/ejercicios/Ejercicio_13.java
@@ -21,16 +21,29 @@ public class Ejercicio_13 {
         System.out.print("Tercer número: ");
         num3 = sc.nextInt();
 
-        if (num1 <= num2) {
-            if (num1 <= num3) {
-                if (num2 <= num3) {
-                    
-                }
-            }
+        // num1: 9
+        // num2: 5
+        // num3: 1
+
+        if (num1 > num2) { // num1: 5 - num2: 9
+            aux = num1;
+            num1 = num2;
+            num2 = aux;
+        }
+        if (num1 > num3) { // num1: 1 - num3: 5
+            aux = num1; //aux: 5
+            num1 = num3; // num1: 1
+            num3 = aux; // num3: 5
+        }
+        if (num2 > num3) { // num2: 5 - num3: 9
+            aux = num2; // aux: 9
+            num2 = num3; // num2: 5
+            num3 = aux; // num3: 9
         }
 
+        System.out.print(num1 + " " + num2 + " " + num3);
 
-
+        sc.close();
 
     }
 }
