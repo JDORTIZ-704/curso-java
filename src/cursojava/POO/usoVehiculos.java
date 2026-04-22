efbd08c134e1c4f143da193c9ce5c8ff4171b838
Juan David Ortiz Forero <e_jdforero@stefanini.com>
2026-04-22 16:02:56 -0500

    Clase #1 de POO

---
+++ src/cursojava/POO/usoVehiculos.java
@@ -0,0 +1,14 @@
+package cursojava.POO;
+
+public class usoVehiculos {
+
+    public static void main(String[] args) {
+
+         vehiculos miCoche = new vehiculos();
+
+         miCoche.setColor();
+
+         System.out.println(miCoche.getColor());
+
+    }
+}
